099e5105128a37e613c2374d320b5c56e3854f78
stvogt <stvogtgeisse@gmail.com>
2014-05-04 14:41:27 -0400

    Adding the other source files

---
+++ operations.py
@@ -0,0 +1,377 @@
+#! /usr/bin/python 
+
+import sys, re, os, commands, shutil
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.integrate import simps
+from scipy.integrate import cumtrapz
+sys.path.append("/home/stvogt/bin")
+import cubman
+
+header =  '''
+-----------------------------------------------------------------------
+          IrcMagic: An Open-Source IRC proccesing suit
+                            IrcMagic 0.1
+
+                            S. Vogt-Geisse
+-----------------------------------------------------------------------
+
+'''
+def read_lines(outfile):
+  if not os.path.exists(outfile):
+    print outfile + " does not exist there -> " + os.getcwd()
+    sys.exit(2)
+  
+  outputfile = open(outfile,"r")
+  outputLines = outputfile.readlines()
+  outputfile.close()
+  return outputLines
+
+def simple_plot(x, y):
+  plt.plot(x, y, 'ro')
+  #plt.axhline(y=0, color='red')
+  plt.show()
+
+def fit(x_coord, y_coord,rank):
+  fx_coord = map(float, x_coord)
+  fy_coord = map(float, y_coord)
+  x = np.array(fx_coord)
+  y = np.array(fy_coord)
+  z = np.polyfit(x,y,rank)
+
+  # creat polynomial function
+  p = np.poly1d(z)
+
+  #plot
+  xp = np.linspace(frx_coord[0], frx_coord[-1], 100)
+  plt.plot(x, y, '.', xp, p(xp), '-')
+  plt.show()
+
+
+def neg_derivative(x_coord, y_coord):
+  fx_coord = map(float, x_coord)
+  fy_coord = map(float, y_coord)
+  x = np.array(fx_coord)
+  y = np.array(fy_coord)
+  f = -np.diff(y)/np.diff(x)
+  return (fx_coord[1:], f)
+
+def integrate(x_coord, y_coord):
+  fx_coord = map(float, x_coord)
+  fy_coord = map(float, y_coord)
+  x = np.array(fx_coord)
+  y = np.array(fy_coord)
+  work = simps(y, x)
+  #work = np.trapz(y, x)
+  return work
+
+def unique(List):
+    output = []
+    for x in List:
+      if not x in output:
+        output.append(x)
+    return output
+
+def print_lists(list1,list2,list1_name,list2_name,filename):
+  f = open(filename+'.dat', 'w')
+  f.write(header)
+  f.write("Generated output from print_lists routine\n\n")
+  f.write('%2s %20s %20s' % (" ",  list1_name, list2_name)+'\n' )
+  for j in range(0,len(list1)):
+      f.write('%2d %20f %20f' % (j+1, round(float(list1[j]),5), round(float(list2[j]),5))+'\n')
+  
+
+def general_print(filename, **kwargs):
+  f = open(filename, 'w')
+  f.write(header)
+  f.write("Generated output from print routine\n\n")
+  f.write('%5s' % (" "))
+  for key in kwargs:
+    if key == "Reaction Coordinate":
+      iter_range = len(kwargs[key])
+      f.write('%20s' % (key))
+  for key in kwargs:
+    if key != "Reaction Coordinate":
+      f.write('%15s' % (key))
+  f.write('\n')
+  #f.write('%2d' % (j+1)
+  #print kwargs
+  for j in range(0,iter_range):
+    f.write('%2d' % (j+1)) 
+    for key in kwargs:
+      
+      f.write('%20f' % (round(float(kwargs[key][j]),5)))
+    f.write('\n')
+
+def num_valence_orbs(atom_list):
+  val_dict= {
+      "1"  : 1,
+      "2"  : 2,
+      "3"  : 1,
+      "4"  : 2,
+      "5"  : 3,
+      "6"  : 4,
+      "7"  : 5,
+      "8"  : 6,
+      "9"  : 7,
+      "10" : 8,
+      "11" : 1, 
+      "12" : 2,
+      "13" : 3,
+      "14" : 4,
+      "15" : 5,
+      "16" : 6,
+      "17" : 7,
+      "18" : 8,
+      }
+  val_elec = 0
+  for atom in atom_list:
+    val_elec = val_elec + val_dict[atom]
+  val_orbs = val_elec/2
+  return val_orbs
+
+def general_plot(plotname, ylabel, works, Zeroline, Show, **kwargs):
+  bullets = ['bo',  'rs', 'k^', 'g-', 'm-.','bo',  'rs', 'k^', 'g-', 'm-.']
+  count = 0
+  for key in kwargs:
+    if key == "Reaction Coordinate":
+      x = kwargs[key]
+      plt.xlabel(key)
+  for key in kwargs:
+    if key != "Reaction Coordinate":
+      plt.plot(x, kwargs[key], bullets[count])
+      count = count + 1
+  if Zeroline:
+    plt.axhline(y=0, color = 'red')
+  plt.axvline(x=works[4], color = 'black', linestyle = "dashed" )
+  plt.axvline(x=works[5], color = 'black', linestyle = "dashed" )
+  plt.ylabel(ylabel)
+  plt.savefig(plotname)
+  if Show:
+    plt.show()
+
+
+def general_plot_orbs(plotname, ylabel, orblist, zeroline=False, NoSave = False, **kwargs):
+  bullets = ['bo',  'rs', 'k^', 'g-', 'm-.','bo',  'rs', 'k^', 'g-', 'm-.']
+  keys = []
+  count = 0
+  occupied = False
+  virtual = False
+  for key in kwargs:
+    if key == "Reaction Coordinate":
+      x = kwargs[key]
+      plt.xlabel(key)
+  for key in kwargs:
+    if key != "Reaction Coordinate":
+      keys.append(key)
+
+  if all(float(kwargs[key][0]) < 0 for key in kwargs):
+    keys.sort(key=int, reverse=True)
+    occupied = True
+  else:
+    keys.sort(key=int)
+    virtual = True
+    
+  for orbNum in orblist:
+    if occupied:
+      key = str(int(1 + float(keys[0]) - float(orbNum)))
+    elif virtual:
+      key = str(int(float(orbNum)))
+    else:
+      print "No orbitals found, check your orbital list"
+      sys.exit(1)
+    plt.plot(x, kwargs[key], bullets[count])
+    if zeroline:
+      plt.axhline(y=0, color='black')
+    plt.ylabel(ylabel)
+    plt.savefig(plotname)
+    #plt.savefig(plotname.split('.')[0]+'_'+str(count1)+plotname.split('.')[1])
+    count = count + 1
+  if not NoSave:
+    plt.show()
+
+def general_plot_prop(plotname, ylabel, proplist, works, Zeroline, Show, **kwargs):
+  doc = """ 
+        Takes as input a diccionary, where the keys are the property to be ploted,
+        and the value is a list with the property  value for each point of the reaction
+        coordinate.
+  """
+  bullets = ['bo',  'rs', 'k^', 'g-', 'm-.','bo',  'rs', 'k^', 'g-', 'm-.']
+  keys = []
+  count = 0
+  occupied = False
+  virtual = False
+  for key in kwargs:
+    if key == "Reaction Coordinate":
+      x = kwargs[key]
+      plt.xlabel(key)
+
+  for key in proplist:
+    if key != "Reaction Coordinate":
+      plt.plot(x, kwargs[key], bullets[count])
+      count = count + 1
+  if Zeroline:
+    plt.axhline(y=0, color='black')
+  plt.ylabel(ylabel)
+  plt.axvline(x=works[4], color = 'black', linestyle = "dashed" )
+  plt.axvline(x=works[5], color = 'black', linestyle = "dashed" )
+  plt.savefig(plotname)
+  if Show:
+    plt.show()
+  
+def atom_label(atom_num_list):
+  atom_dict= {
+      "1"  : "H",
+      "2"  : "He",
+      "3"  : "Li",
+      "4"  : "Be",
+      "5"  : "B",
+      "6"  : "C",
+      "7"  : "N",
+      "8"  : "O",
+      "9"  : "F",
+      "10" : "Ne",
+      "11" : "Na", 
+      "12" : "Mg",
+      "13" : "Al",
+      "14" : "Si",
+      "15" : "P",
+      "16" : "S",
+      "17" : "Cl",
+      "18" : "Ar",
+      }
+  atom_label = []
+  for atom_num in atom_num_list:
+    atom_label.append(atom_dict[atom_num])
+  return atom_label
+
+def render_image(cubefile,jmolfile):
+  print "Generating image corresponding to file:  "+cubefile
+  lines=read_lines(jmolfile)
+  f = open("jmol.spt", 'w')
+  for line in lines:
+    if "%full_file_dir" in line:
+      f.write(line.replace('%full_file_dir', os.getcwd()+"/"+cubefile))
+    elif "%image_file" in line:
+      f.write(line.replace('%image_file', cubefile.split(".")[0]+".png"))
+    else:
+      f.write(line)
+  f.close()
+  if not os.path.isdir("orb_images"):
+    os.makedirs("orb_images")
+  os.chdir("orb_images")
+  #print os.getcwd()
+  #input0 = "jmol -n -s ../jmol.spt"
+  input0 = "jmol ../jmol.spt -n" 
+  print os.getcwd()
+  #print input0
+  job0 = commands.getstatusoutput(input0)
+  os.chdir('../')
+
+def generate_cubes(orbitals,fchk,cube_output,sum_orbs):
+  count = 0
+  for i in orbitals: 
+    print "Genrating cube for orbital number: "+str(i)
+    cub_i = "orbitals_"+str(i)+"_"+cube_output
+    input1="cubegen 0 MO="+str(i)+" "+fchk+" "+cub_i+" 0 h" 
+    job1 = commands.getstatusoutput(input1)
+    cubman.sq_cube(cub_i)
+    #print input1
+    #os.system("ulimit -s unlimited")
+    #print job1
+    if sum_orbs:
+      cub0 = "sum_of_all_cubes_"+cube_output
+      if count == 0:
+        print "Generating the cube of the sum accumulation"
+        input2="cubegen 0 MO="+str(i)+" "+fchk+" "+cub0+" 0 h" 
+        job2 = commands.getstatusoutput(input2)
+        cubman.sq_cube(cub0)
+        print input2
+      else:
+        print "Summing cubes "+cub0+" with "+cub_i
+        cubman.sum_total(cub0+"_sq", cub_i+"_sq")
+      count = count +1
+
+def cube_files(orb_range, atom_list, sum_orbs):
+  if os.path.isdir("CHK"):
+    f = open("cube.log", "w")
+    f.write("Log file for cube generation\n\n")
+    if not os.path.isdir("orbitals"):
+      os.makedirs("orbitals")
+    for file_ in os.listdir("CHK"):
+      print "generating checkpoint file for:  "+file_
+      input0 = "formchk "+"CHK/"+file_ 
+      print input0
+      job0 = commands.getstatusoutput(input0)
+      os.chdir("orbitals")
+      print "Entering this directory --> "+os.getcwd()
+      fchk = "../CHK/"+file_.split('.')[0]+".fchk"
+      cube_output = file_.split(".")[0]+".cub"
+      if len(orb_range) == num_valence_orbs(atom_list):
+      #if orb_range == "valence"
+        sum_orbs = True
+      generate_cubes(orb_range,fchk,cube_output,sum_orbs)
+      print "ordering orbital cubes into directories"
+      for i in orb_range: 
+        if not os.path.isdir("orbitals_"+str(i)):
+          os.makedirs("orbitals_"+str(i))
+        os.chdir("orbitals_"+str(i))
+        original = "../orbitals_"+str(i)+"_"+file_.split(".")[0]+".cub_sq" 
+        destiny  = "orbitals_"+str(i)+"_"+file_.split(".")[0]+".cub"
+        if os.path.isfile(original):
+          shutil.move(original, destiny)
+        else:
+          print "Cube file "+original+" not found, please check your checkpoint file"
+          f.write("Cube file "+original+" not found, please check your checkpoint file\n")
+        render_image(destiny,"/home/stvogt/bin/jmol.scripts")
+        os.chdir("../")
+      if sum_orbs:
+        if not os.path.isdir("orb_sum"):
+          os.makedirs("orb_sum")
+        os.chdir("orb_sum")
+        original = "../sum_of_all_cubes_"+cube_output+"_sq"
+        destiny  = "sum_of_all_cubes_"+cube_output
+        if os.path.isfile(original):
+          shutil.move(original, destiny)
+        else:
+          print "Cube file "+original+" not found, please check!"
+          f.write("Cube file "+original+" not found, please check!\n")
+        render_image(destiny,"/home/stvogt/bin/jmol.scripts")
+        os.chdir("../")
+      os.chdir("../")
+    f.close()  
+
+  else:
+    print "No checkpoint folder CHK!"
+    sys.exit(1)
+
+def valence_density():
+  pass
+
+
+#def general_plot(plotname, ylabel, works, zeroline=False, blocks = 3, **kwargs):
+#  count = 0
+#  count1 = 0
+#  bullets = ['bo',  'rs', 'k^', 'g-', 'm-.','bo',  'rs', 'k^', 'g-', 'm-.']
+#  for key in kwargs:
+#    if key == "Reaction Coordinate":
+#      x = kwargs[key]
+#      plt.xlabel(key)
+#  for key in kwargs:
+#    if key != "Reaction Coordinate":
+#      plt.plot(x, kwargs[key], bullets[count])
+#      count = count + 1
+#      if count % blocks == 0:
+#        count1 = count1+1
+#        if zeroline:
+#          plt.axhline(y=0, color='black')
+#        plt.ylabel(ylabel)
+#        plt.savefig(plotname.split('.')[0]+'_'+str(count1))
+#        #plt.savefig(plotname.split('.')[0]+'_'+str(count1)+plotname.split('.')[1])
+#        plt.show()
+#  if len(kwargs) < 3:
+#    if zeroline:
+#      plt.axhline(y=0, color='black')
+#    plt.ylabel(ylabel)
+#    plt.savefig(plotname)
+#    plt.show()
---
+++ singlePoint.py
@@ -0,0 +1,199 @@
+import sys,re,math
+sys.path.append("/home/stvogt/qtc/scritps/irc")
+import irc
+import operations as op
+
+def process_occ_energies(occ_energies):
+  processed_energies = []
+  for item in occ_energies:
+    if len(item) > 10:
+      gaussian_fuckup = item.split("-")
+      gaussian_fuckup = filter(None, gaussian_fuckup) 
+      for item_stick in gaussian_fuckup:
+        processed_energies.append("-"+item_stick)
+    else: 
+      processed_energies.append(item)
+  return processed_energies
+
+def get_energy(lines):
+  for line in lines:
+    if "SCF Done:" in line:
+      energy = line.split()[4]
+      return energy
+
+def get_orbitals(lines):
+  # Obtain iteration range within the output file
+  All_energies = []
+  for Nline in range(0,len(lines)):
+    if "The electronic state is" in lines[Nline]:
+      startline=Nline+1
+    if "Molecular Orbital Coefficients:" in lines[Nline]:
+      endline=Nline
+      break
+    elif "Condensed to atoms"  in lines[Nline]:
+      endline=Nline
+      break
+
+  # Extract the orbital energies within these lines
+  Orbitalenergies_temp=[]
+  Virtenergies_temp=[]
+  Occenergies_temp=[]
+
+  Orbitalenergies=[]
+  Virtenergies=[]
+  Occenergies=[]
+
+  for Orbline in range(startline,endline):
+    linelist=lines[Orbline].split()
+    for lineitem in range(4,len(linelist)):
+      orbenergy=linelist[lineitem]
+      if "occ." in lines[Orbline]: 
+        Occenergies_temp.append(orbenergy)
+      if "virt." in lines[Orbline]: 
+        Virtenergies_temp.append(orbenergy)
+      Orbitalenergies_temp.append(orbenergy)	
+
+  Orbitalenergies=process_occ_energies(Orbitalenergies_temp)
+  Virtenergies=process_occ_energies(Virtenergies_temp)
+  Occenergies=process_occ_energies(Occenergies_temp)
+  
+  return (Occenergies,Virtenergies,Orbitalenergies)
+
+def bonddistance(xyz):
+  bondLabel = []
+  bondDistance = []
+  atomLabel = op.atom_label(xyz[0])
+  xCoord = xyz[1]
+  yCoord = xyz[2]
+  zCoord = xyz[3]
+
+  for i in range(0,len(atomLabel)-1):
+    for j in range(i,len(atomLabel)):
+      if i is not j:
+        bondLabel.append(atomLabel[i] + str(i+1) + "-" + atomLabel[j] + str(j+1))
+        bondDistance.append(math.sqrt((float(xCoord[i])-float(xCoord[j]))**2 + (float(yCoord[i])-float(yCoord[j]))**2 + (float(zCoord[i])-float(zCoord[j]))**2))
+  return(bondLabel,bondDistance)
+
+def angles(atomLabel,xCoord,yCoord,zCoord):
+  angleLabel = []
+  angle = []
+  for i in range(0,len(atomLabel)-1):
+    for j in range(i, len(atomLabel)):
+      for k in range(0, len(atomLabel)):
+        if i is j:
+          continue
+        elif i is k:
+          continue
+        elif j is k:
+          continue
+        else:
+          ex_ki = float(xCoord[k]) - float(xCoord[i])
+          ex_kj = float(xCoord[k]) - float(xCoord[j])
+          ey_ki = float(yCoord[k]) - float(yCoord[i])
+          ey_kj = float(yCoord[k]) - float(yCoord[j])
+          ez_ki = float(zCoord[k]) - float(zCoord[i])
+          ez_kj = float(zCoord[k]) - float(zCoord[j])
+          rki = math.sqrt(ex_ki**2.0 + ey_ki**2.0 + ez_ki **2.0)
+          rkj = math.sqrt(ex_kj**2.0 + ey_kj**2.0 + ez_kj **2.0)
+          ex_ki = ex_ki/rki
+          ey_ki = ey_ki/rki
+          ez_ki = ez_ki/rki
+          ex_kj = ex_kj/rkj
+          ey_kj = ey_kj/rkj
+          ez_kj = ez_kj/rkj
+          angle.append(180.0/math.pi*math.acos(ex_ki*ex_kj + ey_ki*ey_kj + ez_ki*ez_kj))
+          angleLabel.append(atomLabel[i] + str(i+1) + "-" + atomLabel[k] + str(k+1) + "-" + atomLabel[j] + str(j+1))
+  return(angleLabel,angle)
+
+
+def get_xyz(lines):
+  xyz_info = []
+  atoms_num = []
+  xyz = []
+  x_coords = []
+  y_coords = []
+  z_coords = []
+  found = False
+  for lineNum in range(0,len(lines)):
+    if "Standard orientation:" in lines[lineNum]:  
+      found = True
+      for lineNum1 in range(lineNum+5,len(lines)):
+        p = re.search('\d+\s*-?\d+.\d+\s*-?\d+.\d+', str(lines[lineNum1]))
+        if p: 
+          if len(lines[lineNum1].split()) == 5:
+            atoms_num.append(lines[lineNum1].split()[1]) # Atom number
+            x_coords.append(lines[lineNum1].split()[2])
+            y_coords.append(lines[lineNum1].split()[3])
+            z_coords.append(lines[lineNum1].split()[4])
+          elif len(lines[lineNum1].split()) == 6:
+            atoms_num.append(lines[lineNum1].split()[1]) # Atom number
+            x_coords.append(lines[lineNum1].split()[3])
+            y_coords.append(lines[lineNum1].split()[4])
+            z_coords.append(lines[lineNum1].split()[5])
+        else:
+          xyz.append(atoms_num)
+          xyz.append(x_coords)
+          xyz.append(y_coords)
+          xyz.append(z_coords)
+          break
+    elif found:
+      break
+  return xyz
+
+def get_last_xyz(lines):
+  xyz_info = []
+  atoms_num = []
+  x_coords = []
+  y_coords = []
+  z_coords = []
+  for lineNum in range(0,len(lines)):
+    if "Standard orientation:" in lines[lineNum]:  
+      xyz = []
+      atoms_num = []
+      x_coords = []
+      y_coords = []
+      z_coords = []
+      print lineNum
+      for lineNum1 in range(lineNum+5,len(lines)):
+        p = re.search('\d+\s*-?\d+.\d+\s*-?\d+.\d+', str(lines[lineNum1]))
+        if p: 
+          if len(lines[lineNum1].split()) == 5:
+            atoms_num.append(lines[lineNum1].split()[1]) # Atom number
+            x_coords.append(lines[lineNum1].split()[2])
+            y_coords.append(lines[lineNum1].split()[3])
+            z_coords.append(lines[lineNum1].split()[4])
+          elif len(lines[lineNum1].split()) == 6:
+            atoms_num.append(lines[lineNum1].split()[1]) # Atom number
+            x_coords.append(lines[lineNum1].split()[3])
+            y_coords.append(lines[lineNum1].split()[4])
+            z_coords.append(lines[lineNum1].split()[5])
+        else:
+          xyz.append(atoms_num)
+          xyz.append(x_coords)
+          xyz.append(y_coords)
+          xyz.append(z_coords)
+          break
+  return xyz
+
+def get_orb_symm(lines):
+  symmetries = []
+  found = False
+  for lineNum in range(0,len(lines)):
+    if "Population analysis using the SCF density." in lines[Nline]:
+      found = True
+      for lineNum1 in range(lineNum+4,len(lines)):
+        linelist=lines[lineNum1].split()
+        if not Virtual in lines[lineNum1]: 
+          if "Occupied" in lines[lineNum1]:
+            for lineitem in range(1,len(linelist)):
+              symmetries.append(lineitem.strip(')').strip('('))
+          else:
+            for lineitem in range(0,len(linelist)):
+              symmetries.append(lineitem.strip(')').strip('('))
+
+    elif found:
+      break
+  return symmetries
+
+def get_level_of_theory(lines):
+  pass
